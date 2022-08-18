4c00f6d998e78c919b6474fd60870ad5696b9004
Oguma1028 <neetoguma@gmail.com>
2022-08-18 14:56:34 +0900

    軽微なリファクタ

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,17 @@
 import styles from "./Main.module.css";
 import { Headline } from "../Headline/Headline";
 import { Links } from "../Links/Links";
+import { useEffect } from "react";
 
 export function Main(props) {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <>
       <main className={styles.main}>
