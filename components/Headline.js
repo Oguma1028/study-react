2b467b93dbf03f1feed9455eeccdf54b6fc35a65
Oguma1028 <neetoguma@gmail.com>
2022-09-19 19:38:37 +0900

    ♻️ Index, Main, HのProps関連のリファクタ

--- components/Headline.js
+++ components/Headline.js
@@ -4,14 +4,12 @@ import { Footer } from "../components/Footer";
 import Links from "../components/Links";
 
 export function Headline(props) {
-  console.log(props.page);
   return (
     <div>
-      <h1 className={styles.title}>{props.title}</h1>
+      <h1 className={styles.title}>{props.page}.js</h1>
 
       <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/{props.page}.js</code>
+        Get started by editing {props.children}
       </p>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
-import { Headline } from "../components/Headline";
-import Links from "../components/Links";
+import { Main } from "../components/Main";
 
-export function Home() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,10 +12,7 @@ export function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Headline title="index page" page="いんでっくす" />
-        <Links />
-      </main>
+      <Main page="Index" />
       <Footer />
     </div>
   );
